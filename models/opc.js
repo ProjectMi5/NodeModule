d77faa868f640ab4a32d5fda72f331fa11f4ecbc
Thomas Frei <mail@thomasfrei.de>
2015-08-27 14:41:34 +0200

    reuse the done modifications

--- models/opc.js
+++ models/opc.js
@@ -2,14 +2,6 @@
  * Created by Thomas on 26.08.2015.
  */
 
-//var config = require('./../config.js');
-var nodeopcua = require('node-opcua');
-
-//var EventEmitter = require("events").EventEmitter;
-//var util = require("util");
-//var assert = require("assert");
-var _ = require("underscore");
-
 var async = require("async");
 var Q = require("q");
 //var md5 = require("md5");
@@ -29,6 +21,8 @@ exports.opcua = opcua;
 
 opcua.prototype.connect = function(endpointurl, callback){
   var self = this;
+
+  var nodeopcua = require('node-opcua');
 
   async.series([ function(cb) {
     // only create Client, if there is none
@@ -193,95 +187,49 @@ opcua.prototype.readQ = function(node){
 };
 
 //==================================================================================
+//========================= Write Element ==========================================
+//==================================================================================
+
+opcua.prototype.write = function(nodeId, value, type, callback){
+  var self = this;
+
+  var nodeopcua = require('node-opcua');
+
+  var nodeData = {
+    nodeId : helper.checkAndCorrectNodeId(nodeId),
+    attributeId : 13,
+    value : new nodeopcua.DataValue({
+      value : new nodeopcua.Variant({
+        dataType : helper.convertDataType(type),
+        value : value
+      })
+    })
+  };
+
+  self.session.write([nodeData], function(err, results){
+    if(err){
+      callback(err, null);
+    }
+    callback(err,results.pop());
+  });
+}
+
+opcua.prototype.writeQ = function(nodeId, value, type){
+  var self = this;
+
+  return Q.promise(function(resolve, reject){
+    self.write(nodeId, value, type, function(err, result){
+      if(!err){
+        resolve(result);
+      } else {
+        reject(err);
+      }
+    });
+  })
+};
+
+//==================================================================================
 //========================= Helper Function ========================================
 //==================================================================================
 
-/**
- * Helper Class
- * @type {{checkAndCorrectNodeId: Function, modifiyNodeArray: Function, addAtributeId: Function, concatNodesAndResults: Function}}
- */
-var helper = {
-  /**
-   * Check NodeId and adds / removes stuff if necessary
-   *
-   * @accepts 'MI5.Queue.Queue0.', 'ns=4;s=MI5.Queue.Queue0'
-   * @param baseNode
-   * @returns {String}
-   */
-  checkAndCorrectNodeId : function(baseNode) {
-    // add . at the end if missing
-    if (baseNode.slice(-1) == '.') {
-      baseNode = baseNode.slice(0, -1);
-    }
-
-    // check for namespace and node-identifier
-    if (baseNode.slice(0, 7) != 'ns=4;s=') {
-      baseNode = 'ns=4;s=' + baseNode;
-    }
-
-    return baseNode;
-  },
-
-  /**
-   * Adds node-opcua specific nodes values: 'node' --> {nodeId: node}
-   *
-   * @param string
-   * @returns object
-   */
-  modifiyNodeArray : function(node) {
-    return {
-      nodeId : node
-    };
-  },
-
-  /**
-   * Add attributeId to node-object array:
-   * {nodeId: node} ==> {nodeId: node, attributeId: 13}
-   *
-   * @param node
-   * @returns {number}
-   */
-  addAtributeId : function(node){
-    node.attributeId = 13;
-    return node;
-  },
-
-  /**
-   * Combines nodes and results to one data array with the structure:
-   * [{"nodeId":"MI5.Module1101.Output.SkillOutput.SkillOutput0.Busy",
-       * "value":0}, {...}, {...}]
-   *
-   * @param nodes :
-   *          nodeId
-   * @param results :
-   *          value
-   * @returns {Array}
-   */
-  concatNodesAndResults : function(nodes, results) {
-    var output = new Array;
-    for (var i = 0; i <= nodes.length; i++) {
-      if (nodes[i] != undefined && results[i] != undefined) {
-        // Check for BadNodeId (value: null, then statusCode)
-        // console.log(nodes[i]);
-        // console.log(results[i]);
-        if (_.isEmpty(results[i].value)) {
-          output[i] = {
-            nodeId : nodes[i].nodeId.value,
-            value : '',
-            error : results[i].statusCode.description
-          };
-        } else {
-          output[i] = {
-            nodeId : nodes[i].nodeId.value,
-            value : results[i].value.value
-          };
-        }
-      }
-    }
-    return output;
-  },
-
-
-
-};
-
+var helper = require('./opc-helper.js').helper;
--- test/test-opcua.js
+++ test/test-opcua.js
@@ -5,58 +5,94 @@ var config = require('./../config.js');
 
 var assert = require('chai').assert;
 
+
 describe('Test OPC UA library', function(){
 
   before(function(done){
-    var opcuaserver = require('./opcua-server');
-    opcuaserver.opcuaserver.then(done);
+    var opcuaserver = require('./mock/opcua-server');
+    opcuaserver.instance().done(done); //wait until server is started
   });
 
   describe('test opcua instance', function(){
     var opc = new require('./../models/opc.js');
     var opcua = new opc.opcua();
 
+    var nodeId = 'MI5.Module2501.Input.SkillInput.SkillInput0.Execute';
+
     describe('#connectQ() connect to mock opc ua server', function() {
-      it('opc.connected should be true', function (done) {
-        opcua.connectQ(config.endpointurl)
+      it('opc.connected should be true', function () {
+        return opcua.connectQ(config.endpointurl)
           .then(function () {
             assert.isTrue(opcua.connected);
-            done();
           });
       });
     });
 
     describe('#readArrayQ() read an array of nodes', function(){
-      it('read one boolean element', function(done){
-        var nodeId = 'MI5.Module2501.Input.SkillInput.SkillInput0.Execute';
-        opcua.readArrayQ([nodeId])
+      it('read one boolean element', function(){
+        return opcua.readArrayQ([nodeId])
           .then(function(results){
+            console.log(results);
+
             assert.isArray(results);
             assert.equal(results[0].nodeId, nodeId);
-            assert.isFalse(results[0].value);
-            done();
+            assert.isFalse(results[0].value, 'must be false');
           });
       });
     });
 
     describe('#readQ()', function(){
-      it('read one element', function(done){
-        var nodeId = 'MI5.Module2501.Input.SkillInput.SkillInput0.Execute';
-        opcua.readQ(nodeId)
+      it('read one element', function(){
+        return opcua.readQ(nodeId)
           .then(function(result){
+            console.log(result);
+
             assert.equal(result.nodeId, nodeId);
             assert.isFalse(result.value);
-            done();
+          });
+      });
+    });
+
+    describe('#write()', function(){
+      it('should overwrite execute with true', function(done){ //callback needed, since write is cb function
+        opcua.write(nodeId, true, 'Boolean', function(err, result){
+          console.log(err, result);
+
+          assert.isNull(err);
+          assert.equal(result.name,'Good');
+
+          // Test
+          opcua.readQ(nodeId)
+            .then(function(result){
+              assert.isTrue(result.value);
+            }).done(done); // catch exceptions caught in the promise chain by envoking final done callback
+        });
+      });
+    });
+
+    describe('#writeQ()', function(){
+      it('should overwrite execute with false', function(){
+        return opcua.writeQ(nodeId, true, 'Boolean')
+          .then(function(result){
+            assert.equal(result.name,'Good');
+
+            // Test
+            return opcua.readQ(nodeId)
+              .then(function(result){
+                assert.isTrue(result.value);
+              });
+          })
+          .fail(function(err){
+            assert.isNull(err);
           });
       });
     });
 
     describe('#disconnectQ() from mock opc ua server', function(){
-      it('opc.connected should be false', function(done){
-        opcua.disconnectQ()
+      it('opc.connected should be false', function(){
+        return opcua.disconnectQ()
           .then(function() {
             assert.isFalse(opcua.connected, 'no connection');
-            done();
           });
       });
     });
