223da05b0196eed2c0438e52eb4c573f32dc7863
Thomas Frei <mail@thomasfrei.de>
2015-09-17 14:57:29 +0200

    updated sample config

--- config.sample.js
+++ config.sample.js
@@ -2,7 +2,12 @@
  * Sample Configuration
  */
 var config = {
-  test: 'test',
+  moduleid  : 2501,
+  skillid   : 1402,
+  position  : 1345,
+  moduleName: 'Input Module',
+
+  endpointurl : 'opc.tcp://localhost:4334/',
 };
-	
+
 module.exports = config;
